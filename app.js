b460d66153320801ac148d4ee530820fe9f2f3c7
jade <hendricksjade@gmail.com>
2020-04-22 03:53:22 +0200

    adding in intial user routes

--- app.js
+++ app.js
@@ -94,6 +94,42 @@ const deleteTour = (req, res) => {
   })
 }
 
+const getAllUsers = ((req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not yet defined"
+  })
+});
+
+const getUser = ((req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not yet defined"
+  })
+});
+
+const createUser = ((req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not yet defined"
+  })
+});
+
+const updateUser = ((req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not yet defined"
+  })
+});
+
+const deleteUser = ((req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not yet defined"
+  })
+});
+
+
 //requests
 // app.get("/api/v1/tours", getAllTours);
 // app.get("/api/v1/tours/:id", getTour);
@@ -112,6 +148,16 @@ app
   .patch(updateTour)
   .delete(deleteTour);
 
+app
+  .route("/api/v1/users")
+  .get(getAllUsers)
+  .post(createUser);
+
+app
+  .route("/api/v1/users/:id")
+  .get(getUser)
+  .patch(updateUser)
+  .delete(deleteUser);
 //starting a server
 const port = 3000;
 app.listen(port, () => {
