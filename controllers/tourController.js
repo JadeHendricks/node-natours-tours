0f3d2d73db72a6659196bb12a98405efa9046f85
jade <hendricksjade@gmail.com>
2020-04-28 17:05:41 +0200

    rafactoring api features

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,4 +1,5 @@
 const Tour = require("../models/tourModel");
+const APIFeatures = require("../utilities/apiFeatures");
 //Blocking Code - Testing
 // const tours = JSON.parse(fileSystem.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`));
 
@@ -6,77 +7,20 @@ exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
   req.query.sort = "-ratingsAverage,price";
   req.query.fields = "name,price,ratingsAverage,summary,difficulty";
-  
+
   next();
 }
 
 exports.getAllTours = async (req, res) => {
-  //gets all the params in the URL and stores it in an object
-  // console.log(req.query);
   try {
-    //1A) Filtering
-        // const tours = await Tour.find()
-    //   .where("duration")
-    //   .equals(5)
-    //   .where("difficulty")
-    //   .equals("easy");
-    //creating a hardcopy and not a reference
-    const queryObj = {...req.query};
-    const excludedFields = ["page", "sort", "limit", "fields"];
-    //looping through here to remove the excluded files from the queryObj
-    excludedFields.forEach(element => delete queryObj[element]);
+    //Execute Query
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
 
-
-
-    //1B) advanced Filtering
-    //convert the object to a string and replace the gte, gt, lte,le
-    let queryString = JSON.stringify(queryObj);
-    queryString = queryString.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-
-    //get all tours from the database
-    let query = Tour.find(JSON.parse(queryString));
-
-
-
-    // 2) Sorting
-    if (req.query.sort) {
-      //mongo requests the names to be a string and seperated by spaces
-      const sortBy = req.query.sort.split(',').join(" ");
-      // console.log("req.query", req.query);
-      // console.log("sortBy", sortBy);
-      //sort via price, this is set on the query
-      query = query.sort(req.query.sort);
-    } else {
-      //decending order, newest one goes first
-      query = query.sort("-createdAt");
-    }
-
-    // 3) Field limiting
-    if (req.query.fields) {
-      //mongo requests the names to be a string and seperated by spaces
-      const fields = req.query.fields.split(",").join(" ");
-      //select includes
-      query = query.select(fields);
-    } else {
-      //using a - in select is excluding
-      query = query.select("-__v");
-    }
-
-    // 4) pagination
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
-    //calculation skip to add to the skip method
-    const skip = (page - 1) * limit;
-    //page=3&limit=10, 1-10, page 1, 11 - 20, page 2, 12 - 30, page 3
-    query = query.skip(skip).limit(limit);
-
-    if (req.query.page) {
-      //this is going to return the number of documents
-      const numberOfTours = await Tour.countDocuments();
-      if (skip >= numberOfTours) throw new Error("This page does not exsist");
-    }
-
-    const tours = await query;
+    const tours = await features.query;
 
     res.status(200).json({
       status: "success",
