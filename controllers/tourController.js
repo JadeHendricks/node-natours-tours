7adb1b47c1c35c1bb2e63c8a390742b53ba17fdf
jade <hendricksjade@gmail.com>
2020-04-28 15:22:14 +0200

    sorting and filtering sections covered

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,9 +3,44 @@ const Tour = require("../models/tourModel");
 // const tours = JSON.parse(fileSystem.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`));
 
 exports.getAllTours = async (req, res) => {
+  //gets all the params in the URL and stores it in an object
+  // console.log(req.query);
   try {
+    //1A) Filtering
+    //creating a hardcopy and not a reference
+    const queryObj = {...req.query};
+    const excludedFields = ["page", "sort", "limit", "fields"];
+    //looping through here to remove the excluded files from the queryObj
+    excludedFields.forEach(element => delete queryObj[element]);
+
+    //1B) advanced Filtering
+    //convert the object to a string and replace the gte, gt, lte,le
+    let queryString = JSON.stringify(queryObj);
+    queryString = queryString.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+
     //get all tours from the database
-    const tours = await Tour.find();
+    let query = Tour.find(JSON.parse(queryString));
+
+    // 2) Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(" ");
+      // console.log("req.query", req.query);
+      // console.log("sortBy", sortBy);
+      //sort via price, this is set on the query
+      query = query.sort(req.query.sort);
+    } else {
+      //decending order, newest one goes first
+      query = query.sort("-createdAt");
+    }
+
+
+    // const tours = await Tour.find()
+    //   .where("duration")
+    //   .equals(5)
+    //   .where("difficulty")
+    //   .equals("easy");
+
+    const tours = await query;
 
     res.status(200).json({
       status: "success",
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-dotenv.config({path: "./config.env"});
 //only needs to happen once to use globally and before app
 // console.log(process.env);
+dotenv.config({path: "./config.env"});
+
 const app = require("./app");
 
 const DB = process.env.DATABASE.replace("<PASSWORD>", process.env.DATABASE_PASSWORD);
