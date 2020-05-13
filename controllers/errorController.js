3c1c95a31c6f7748bc104f0f6bd376e78560b978
jade <hendricksjade@gmail.com>
2020-05-13 15:38:10 +0200

    handling invalid database ids and duplicate database fields

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,58 @@
-module.exports = (err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || "error";
+const AppError = require('../utilities/appError');
 
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = (err) => {
+  //find text that is in the errmsg in quotes REGEX
+  const value = err.errmsg.match(/(["'])(?:(?=(\\?))\2.)*?\1/)[0];
+  const message = `Duplicate field value: ${value}. Please use another value`;
+
+  console.log(value);
+  return new AppError(message, 400);
+};
+
+const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
-    message: err.message
+    error: err,
+    message: err.message,
+    stack: err.stack,
   });
 };
+
+const sendErrorProd = (err, res) => {
+  //errors from the operational code
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+    //Programming or other unknown error: don't leak details to the client
+  } else {
+    // Log error to the console
+    console.error('Error', err);
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong',
+    });
+  }
+};
+
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    let error = { ...err };
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    sendErrorProd(error, res);
+  }
+};
+
+// in production we want to leak as little infor about errors as possible this is the complete opposite for development
