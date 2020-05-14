41f1d0f2908afc20d90a5e1639f89630f71ad529
jade <hendricksjade@gmail.com>
2020-05-14 23:43:10 +0200

    password rest functionality and nodemailer integration

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,9 @@
 const { promisify } = require('util');
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
-const catchAsync = require('../utilities/catchAsync');
 const AppError = require('../utilities/appError');
+const catchAsync = require('../utilities/catchAsync');
+const sendEmail = require('../utilities/email');
 
 const signToken = (id) => {
   return jwt.sign({ id: id }, process.env.JWT_SECRET, {
@@ -115,7 +116,53 @@ exports.restrictTo = (...roles) => {
         new AppError('You do not have permission to perform this action', 403)
       );
     }
-
     next();
   };
+};
+
+exports.forgotPassword = catchAsync(async (req, res, next) => {
+  // 1) get user based on posted email
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    return next(new AppError('There is no user with that email address', 404));
+  }
+  // 2) generate the random reset token
+  const resetToken = user.createPasswordResetToken();
+  //we need to save our changes here
+  await user.save({ validateBeforeSave: false }); //this will deactivate all the validators that we declared in our schema
+  // 3) send it back as an email
+  const resetURL = `${req.protocol}://${req.get(
+    'host'
+  )}/api/v1/users/resetPasword/${resetToken}`;
+
+  const message = `Forgot your password, submit a PATCH request with your new password and password confirm to : ${resetURL}. \n
+  if you didn't forget your password, please ignore this email!`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: 'Your password reset token (valid for 10 min)',
+      message: message,
+    });
+
+    res.status(200).json({
+      status: 'success',
+      message: 'Token sent to email!',
+    });
+  } catch (err) {
+    user.passwordResetToken = undefined;
+    user.passwordRestExpires = undefined;
+    await user.save({ validateBeforeSave: false }); //this will deactivate all the validators that we declared in our schema
+
+    return next(
+      new AppError(
+        'There was an error sending the email, try again later!',
+        500
+      )
+    );
+  }
+});
+
+exports.resetPassword = (req, res, next) => {
+  next();
 };
