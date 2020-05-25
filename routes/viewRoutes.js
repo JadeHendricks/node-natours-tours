0374ae3ce95790388e310dce68cc2491b0d8a19a
jade <hendricksjade@gmail.com>
2020-05-25 16:46:38 +0200

    building the login screen

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const { getOverview, getTour } = require('../controllers/viewsController');
+const {
+  getOverview,
+  getTour,
+  getLoginForm,
+} = require('../controllers/viewsController');
 
 router.get('/', getOverview);
 router.get('/tour/:slug', getTour);
+router.get('/login', getLoginForm);
 
 module.exports = router;
