b9c2ee3dec3287a60c1ceca703220ecafea901d9
jade <hendricksjade@gmail.com>
2020-05-27 17:29:38 +0200

    rendering the error pages

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -22,6 +22,10 @@ exports.getTour = catchAsync(async (req, res, next) => {
     fields: 'review rating user',
   });
 
+  if (!tour) {
+    return next(new AppError('There is no tour with that name.', 404));
+  }
+
   // 2) Build template
   // 3) Render template from step 1
   res.status(200).render('tour', {
